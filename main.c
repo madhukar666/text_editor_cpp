7cd06accd53e5294a44d4fb6b365fc743c200cbd
madhukar666 <anveshnallamadha@gmail.com>
2025-05-23 16:32:28 +0530

    canonical mode

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 
-
+#include <unistd.h>
 #include <stdio.h>
 
 int main(){
 
-    printf("First commit\n");
-
+    char c;
+    /* currently terminal is in canonical mode i.e., it reads the characters and only takes the read content after the
+    user clicks enter i mean it doesn't act the way text editors do
+    It stops reading after it encounyters the delimiter '~'*/
+    while(read(STDIN_FILENO,&c,1) == 1 && c!='~');
     return 0;
-    
+
 }
